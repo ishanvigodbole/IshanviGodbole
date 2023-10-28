f9e008db3e1941dd03b069a55da1a93d61390f89
ishanvigodbole <149247294+ishanvigodbole@users.noreply.github.com>
2023-10-28 20:06:57 +0530

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
+public class employee
+{
+    public string name;
+    public string city;
+    public int salary;
+    public int DA;
+    public int HRA;
+
+    static public void getdata()
+    {
+        Console.WriteLine(" enter name: ");
+        string name=Console.ReadLine(); 
+       
+        Console.WriteLine("enter your city");
+        string city=Console.ReadLine();
+        Console.WriteLine("enter your salary");
+        int salary=ConvertToInt32(Console.ReadLine());
+        Console.WriteLine("enter your DA");
+        int DA= ConvertToInt32(Console.ReadLine());
+        Console.WriteLine("enter your HRA");
+        int HRA= ConvertToInt32(Console.ReadLine());
+    }
+
+    static public int calculate()
+    {
+        int total= salary + salary * DA/ 100 + salary * HRA / 100;
+        return total;
+    }
+
+    static public void Display()
+    { Console.WriteLine(int total); }
+}
+ employee employee=new employee();
+ int employee.getdata();
+int employee.calculate( );
+ int employee.display(int total);
---
+++ program1.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[]a = [21, 22, 67, 43, 12, 4];
+for (int i = 0; i < 6; i++)
+{
+    int temp = a[i];
+    a[i] = a[6 - i - 1];
+    a[6 - i - 1] = temp;
+}
+for (int i = 0; i < 6; i++)
+{
+
+    Console.WriteLine(a[i]);
+}
